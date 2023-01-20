519a78b9db1df1e40fa6f396a0d8e68cb59a7411
diegoe120 <38639302+diegoe120@users.noreply.github.com>
2023-01-20 10:36:19 -0300

    menu funcionando

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-// Declare una variable para almacenar el estado del menú
-var menuIsVisible = false;
-
-// Asocia el evento "click" al botón
-document.querySelector(".btn-menu").addEventListener("click", function () {
-  // Obtiene el elemento del menú
-  var menu = document.querySelector("#menu");
-
-  // si el menu esta invisible
-  if (!menuIsVisible) {
-    menu.style.display = "block"; //lo hacemos visible
-    menuIsVisible = true; //cambiamos el estado
+function manejoMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
   } else {
-    menu.style.display = "none"; //lo hacemos invisible
-    menuIsVisible = false; //cambiamos el estado
+    menu.style.display = "block";
   }
-});
-
-menu();
+}
